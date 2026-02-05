6fe8f19569a7d9e2d308ce132fa5b48462a5c7ed
ThisIsTheCatalyst <mayank7.programming@gmail.com>
2026-02-05 22:57:08 +0530

    Updated redis_client.py to not query password

--- redis_client.py
+++ redis_client.py
@@ -6,14 +6,11 @@ import redis
 # ----------------------------
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = int(os.environ["REDIS_PORT"])
-REDIS_PASSWORD = os.environ["REDIS_PASSWORD"]
-
 # ----------------------------
 # Redis client (singleton)
 # ----------------------------
 redis_client = redis.Redis(
     host=REDIS_HOST,
     port=REDIS_PORT,
-    password=REDIS_PASSWORD,
     decode_responses=True  # IMPORTANT: returns strings, not bytes
 )
