3daa6907fe84a95239a5f799afb2bb46881b58e4
Bhoomi Saraf <13bhoomisaraf65@gmail.com>
2026-02-15 23:31:12 +0530

    Update redis_client.py

--- redis_client.py
+++ redis_client.py
@@ -1,11 +1,7 @@
 import os
 import redis
 
-# Get Redis configuration from environment
-REDIS_HOST = os.environ.get("REDIS_HOST", "localhost")
-REDIS_PORT = int(os.environ.get("REDIS_PORT", 6379))
-REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", None)
-API_SECRET = os.getenv("API_SECRET")
+redis_client = redis.from_url(os.getenv("REDIS_URL"))
 # Create Redis client with password support
 redis_client = redis.Redis(
     host=REDIS_HOST,
